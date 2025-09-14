ab3d3a2e4cea037ef99669320356a0c3622ff28a
brkyldrm030 <burak.yildirim030@outlook.de>
2025-09-14 21:06:54 +0200

    x-clip

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Aysulix-Design/", // <<< wichtig für GitHub Pages Unterpfad
+  base: "/Aysulix-Design/", // 👈 ganz wichtig für GitHub Pages
 });
